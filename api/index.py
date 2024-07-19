9d66a80f63171ebbdc72526f9c83ba8448a7c794
Antx Stellar <148527164+plsyouarenotalone@users.noreply.github.com>
2024-07-19 10:55:14 +0700

    Update index.py

--- api/index.py
+++ api/index.py
@@ -8,24 +8,3 @@ class handler(BaseHTTPRequestHandler):
         self.end_headers()
         self.wfile.write('Hello, world!'.encode('utf-8'))
         return
-        # Kalkulator Penghitung Luas Lingkaran
-
-# Deklarasi Variabel phi
-phi = 3.14 # Dini Ga Bisa Pake tanda koma harus pake titik
-
-# Header/Judul
-print("Kalkulator Menghitung Luas Lingkaran")
-print()
-
-# Input Dari User
-r = float(input("Masukkan Jari-jari: ")) # User Input
-
-# Penghitungan
-luas = phi * r * r # Rumus Luas Lingkaran
-luas_bulat = round(luas, 2) # Pembulatan Angka
-
-# Output Ke User
-print("Luas Lingkaran Adalah: ", luas_bulat)
-
-
-
